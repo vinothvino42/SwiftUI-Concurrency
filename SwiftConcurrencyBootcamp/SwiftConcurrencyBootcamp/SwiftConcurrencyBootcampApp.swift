7831720715537f8ea1c36914b62319f81f084072
vinothvino42 <vinothvino42@gmail.com>
2023-08-29 09:16:31 +0530

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Vinoth Vino on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
